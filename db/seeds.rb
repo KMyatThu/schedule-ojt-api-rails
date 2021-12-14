3d02fb154dde4fed8f36d192f72bc1fc3aeacee8
KMyatThu <scm.kaungmyatthu@gmail.com>
2021-12-14 22:49:46 +0630

    add seed file and fix puma

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {
+    name: 'Kaung Myat',
+    email: 'kaungmyat@gmail.com',
+    password: 'password123',
+    profile: 'default_profile.jpg',
+    role: 0,
+    phone: '09123123',
+    address: 'Yangon.',
+    dob: '2012-02-07',
+    create_user_id: '1',
+    updated_user_id: '1'
+  },
+  {
+    name: 'Mg Mg',
+    email: 'mgmg@gmail.com',
+    password: 'password123',
+    profile: 'default_profile.jpg',
+    role: 0,
+    phone: '09123124',
+    address: 'Yangon.',
+    dob: '2012-02-07',
+    create_user_id: '1',
+    updated_user_id: '1'
+  },
+  {
+    name: 'Zaw Zaw',
+    email: 'zawzaw@gmail.com',
+    password: 'password123',
+    profile: 'default_profile.jpg',
+    role: 1,
+    phone: '09123125',
+    address: 'Yangon.',
+    dob: '2012-02-07',
+    create_user_id: '1',
+    updated_user_id: '1'
+  },
+])
